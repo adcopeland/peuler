67bf33c36bd0115fca7b8d9b3f579df6ee30e3e8
adam <adcopejunk@gmail.com>
2020-01-11 23:31:02 -0600

    Added lib to rust

--- rust/peuler/src/bin/p7.rs
+++ rust/peuler/src/bin/p7.rs
@@ -4,19 +4,10 @@ fn main() {
 
   while prime_idx < 10001 {
     current += 1;
-    if is_prime(current) {
+    if peuler::is_prime(current) {
       prime_idx += 1;
     }
   }
 
   println!("{}", current);
-}
-
-fn is_prime(n: i32) -> bool {
-  for i in 2..=n/2 {
-    if n % i == 0 {
-      return false;
-    }
-  }
-  true
 }
---
+++ rust/peuler/src/lib.rs
@@ -0,0 +1,10 @@
+pub fn is_prime(n: i32) -> bool {
+  let mut i = 2;
+  while i < n/i {
+    if n % i == 0 {
+      return false;
+    }
+    i += 1;
+  }
+  true
+}
